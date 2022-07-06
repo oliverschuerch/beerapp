2d9ae0fed2b2fa9c35931031e2a6b0d23387ac38
Oliver Schürch <oli@skaos.ch>
2022-07-06 12:17:29 +0200

    Removes CSS Extraction

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  css: {
-    extract: true,
-    sourceMap: true
-  },
-
   assetsDir: 'assets',
   outputDir: 'dist'
 })
